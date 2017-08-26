c6ccc3c99bb18ee75aa3b5e93d7b83dfb094dee8
stoicism <ems5311@gmail.com>
2017-08-25 20:45:03 -0700

    function update

--- cs/FunctionHandler.cs
+++ cs/FunctionHandler.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Function
 {
     public class FunctionHandler
     {
-        public void Handle(string input) {
-            Console.WriteLine("Hi there - your input was: "+ input);
+        public void Handle(string input)
+        {
+            string value = "";
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                dynamic jsonResponse = JsonConvert.DeserializeObject(input);
+                if (jsonResponse.value != null)
+                {
+                    value = (string)jsonResponse.value;
+                }
+            }
+
+            //var output = new { factors = GetFactors(value) };
+            Console.WriteLine(JsonConvert.SerializeObject(value));
+        }
+
+        private int[] GetFactors(int value)
+        {
+            return Enumerable.Range(1, value).Where(i => value % i == 0).ToArray();
         }
     }
 }
